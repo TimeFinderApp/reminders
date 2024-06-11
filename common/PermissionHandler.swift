d372c01b1d0e7954bbfca9bb64550a08948727d3
Manuel Zamora <manuel.zamora.86@gmail.com>
2024-06-11 15:08:13 -0500

    Code Refactored on PermissionHandler file

--- common/PermissionHandler.swift
+++ common/PermissionHandler.swift
@@ -9,29 +9,30 @@ enum PermissionStatus: String {
     case fullAccess = "fullAccess"
     case writeOnly = "writeOnly"
 
-    // Existing initializer for cases up to iOS 16 and macOS 13
     init(status: EKAuthorizationStatus) {
         switch status {
-        case .authorized: self = .authorized
-        case .denied: self = .denied
-        case .notDetermined: self = .notDetermined
-        case .restricted: self = .restricted
-        @unknown default: self = .unknown
-        }
-    }
-
-    // iOS 17 and macOS 14 specific initializer
-    @available(iOS 17.0, macOS 14.0, *)
-    init(newStatus status: EKAuthorizationStatus) {
-        switch status {
-        case .authorized: self = .authorized
-        case .denied: self = .denied
-        case .notDetermined: self = .notDetermined
-        case .restricted: self = .restricted
-        // Handle the new cases specific to iOS 17 and macOS 14
-        case .fullAccess: self = .fullAccess
-        case .writeOnly: self = .writeOnly
-        @unknown default: self = .unknown
+        case .authorized:
+            self = .authorized
+        case .denied:
+            self = .denied
+        case .notDetermined:
+            self = .notDetermined
+        case .restricted:
+            self = .restricted
+        default:
+            // Handle the new cases specific to iOS 17 and macOS 14
+            if #available(iOS 17.0, macOS 14.0, *) {
+                switch status {
+                case .fullAccess:
+                    self = .fullAccess
+                case .writeOnly:
+                    self = .writeOnly
+                default:
+                    self = .unknown
+                }
+            } else {
+                self = .unknown
+            }
         }
     }
 }
@@ -42,10 +43,6 @@ class PermissionManager {
 
     static func getPermissionStatus() -> PermissionStatus {
         let status = EKEventStore.authorizationStatus(for: .reminder)
-        if #available(iOS 17.0, macOS 14.0, *) {
-            return PermissionStatus(newStatus: status)
-        } else {
-            return PermissionStatus(status: status)
-        }
+        return PermissionStatus(status: status)
     }
 }
